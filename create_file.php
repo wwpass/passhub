aee03d658c76202dd91d4a2d90fcf6cc5a487e02
Mikhail Vysogorets <m.vysogorets@wwpass.com>
2023-05-16 23:13:43 -0400

    code cleanup

--- create_file.php
+++ create_file.php
@@ -8,7 +8,7 @@
  * @category  Password_Manager
  * @package   PassHub
  * @author    Mikhail Vysogorets <m.vysogorets@wwpass.com>
- * @copyright 2016-2018 WWPass
+ * @copyright 2016-2023 WWPass
  * @license   http://opensource.org/licenses/mit-license.php The MIT License
  */
 
@@ -44,34 +44,25 @@ function create_file_item_proxy($mng) {
     $SafeID = $_POST['vault'];
     $UserID = $_SESSION['UserID'];
 
+    /*
     if (isset($_POST['check'])) {
         $user = new User($mng, $UserID);
         if ($user->canWrite($SafeID) == false) {
             return "Sorry, you do not have editor rights for this safe";
         }
-        $result = [];
-        if (defined('MAX_STORAGE_PER_USER')) {
-            $result['maxStorage'] = MAX_STORAGE_PER_USER;
-        } 
-        if (isset($_SESSION['plan'])  && ($_SESSION['plan'] == "FREE") && defined('FREE_ACCOUNT_MAX_STORAGE')) {
-            $result['maxStorage'] = FREE_ACCOUNT_MAX_STORAGE;
-        }
-        if (!defined('MAX_FILE_SIZE')) {
-            $result['maxFileSize'] = 5 * 1024 * 1024;
-        } else {
-            $result['maxFileSize'] = MAX_FILE_SIZE;
-        }
+        $result = $user->getPlanDetails();
         $result['storageUsed'] = $_SESSION['STORAGE_USED'];
-        $result['status'] = 'Ok';
         return $result;
     }
+    */
 
     $folder = isset($_REQUEST['folder'])? $_REQUEST['folder'] : 0;
 
     $meta = $_POST['meta'];
     $file = $_POST['file'];
 
-    $fileContent = file_get_contents($_FILES['blob']['tmp_name']);
+    $fileContent = file_get_contents($_FILES['blob']['tmp_name']);  // writes into /tmp
+    
     if(!$fileContent) {
         Utils::err("file upload error 76");
         return(["status" => "Internal server error 76. Please try again later."]);
@@ -91,6 +82,9 @@ function create_file_item_proxy($mng) {
 $result = create_file_item_proxy($mng);
 if (gettype($result) == "string") {
     $result = array("status" => $result);
+}
+if($result['status'] != "Ok") {
+    Utils::err("File creation error " . $result['status']);
 }
 // Prevent caching.
 
