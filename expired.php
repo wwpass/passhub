99f45db31162c5b1690cdb9a6d835d3948b60056
Mikhail Vysogorets <m.vysogorets@wwpass.com>
2023-09-29 19:26:58 -0400

    code cleanup

--- expired.php
+++ expired.php
@@ -1,32 +1,6 @@
 <?php
 
-/**
- * new.php
- *
- * PHP version 7
- *
- * @category  Password_Manager
- * @package   PassHub
- * @author    Mikhail Vysogorets <m.vysogorets@wwpass.com>
- * @copyright 2016-2018 WWPass
- * @license   http://opensource.org/licenses/mit-license.php The MIT License
- */
+ header("Location: logout.php");
+ exit();
 
-require_once 'config/config.php';
-require_once 'vendor/autoload.php';
-
-use PassHub\Utils;
-use PassHub\DB;
-
-$mng = DB::Connection();
-
-session_start();
-session_destroy();
-
-echo Utils::render(
-    'expired.html',
-    [
-      'narrow' => true,
-      'PUBLIC_SERVICE' => defined('PUBLIC_SERVICE') ? PUBLIC_SERVICE : false
-    ]
-);
+ 
