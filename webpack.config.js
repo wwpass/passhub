c05412b6e27d5d029e0517722d2ffbfa4ec7f29c
Mikhail Vysogorets <m.vysogorets@wwpass.com>
2025-03-04 13:58:43 -0500

    code cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,14 @@ const path = require('path');
 module.exports = {
 
   entry: {
-//    index: './src/js/index.js',
-//    iam: './src/js/iam.js',
+    //    index: './src/js/index.js',
+    //    iam: './src/js/iam.js',
     upsert_user: './src/js/upsert_user.js',
     login: './src/js/login.js',
-//    payment: './src/js/payment.js',
-//    survey: './src/js/survey.js',
+    //    payment: './src/js/payment.js',
+    //    survey: './src/js/survey.js',
+    test_forge: './src/js/test_forge.js',
+
   },
 
   output: {
@@ -17,14 +19,14 @@ module.exports = {
   },
 
   externals: {
-//    jquery: 'jQuery',
+    //    jquery: 'jQuery',
   },
   devtool: 'source-map',
   module: {
     rules: [{
       test: /\.js$/,
       // exclude: /node_modules/,
-      loader: 'babel-loader' 
+      loader: 'babel-loader'
     }]
-  } 
+  }
 };
